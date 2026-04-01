a6f07a6a7d421af05c9d36cf37b181532fddf67d
GustavoHenriqueC07 <ghenriquexzs@gmail.com>
2026-04-01 10:13:39 -0300

    Feito por Gh

---
+++ ex05/script compra copy.js
@@ -0,0 +1,9 @@
+let multiplo=Number(prompt("Digite um numero"));
+
+if (multiplo % 5 === 0  ){
+    alert ("Seu numero é multiplo de 5");
+
+}
+else {
+    alert("Seu numero não é multiplo de 5");
+}
