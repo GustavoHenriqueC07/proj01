afdf6d99e947a6660ad9f5108534bd46e6e2eb0c
GustavoHenriqueC07 <ghenriquexzs@gmail.com>
2026-04-01 10:12:55 -0300

    Feito por Gustavo

---
+++ ex04/script compra.js
@@ -0,0 +1,8 @@
+let Valor= Number(prompt("Digite o valor da sua compra"));
+
+if(Valor >= 100){
+    alert ("Parabéns, você recebeu 10% de desconto")
+}
+else {
+alert("Você pagará o valor total da compra")
+}
