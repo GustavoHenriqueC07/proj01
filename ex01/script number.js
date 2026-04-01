502c3d7e12386ce4623c28344c599ebc693571eb
GustavoHenriqueC07 <ghenriquexzs@gmail.com>
2026-04-01 10:04:57 -0300

    Feito por Gustavo Henrique

---
+++ ex01/script number.js
@@ -0,0 +1,13 @@
+let Number= Number(prompt("DigiTe um Número"));
+
+if (number > 0) {
+  alert ("Numero Positivo");  
+} 
+else if ( Number = 0) {
+  alert ("Numero neutro");
+}
+
+
+else {
+    alert ("Numero Negativo");
+}
